0acb8b13b2ef6191803d079daea7a11e3a51fe15
Ruihang Lai <ruihangl@cs.cmu.edu>
2023-04-18 00:00:50 -0400

    Apply optim for "decode + transpose"

--- web_llm/transform/dispatch_tir_operator.py
+++ web_llm/transform/dispatch_tir_operator.py
@@ -1934,6 +1934,61 @@ def decode8(rxplaceholder: T.Buffer((T.int64(1376), T.int64(4096)), "uint32"), r
             T_transpose[v_ax0, v_ax1] = decode[v_ax1, v_ax0]
 
 
+@T.prim_func
+def decode4_fp16(rxplaceholder: T.Buffer((T.int64(512), T.int64(4096)), "uint32"), rxplaceholder_1: T.Buffer((T.int64(128), T.int64(4096)), "float16"), rxplaceholder_2: T.Buffer((T.int64(128), T.int64(4096)), "float16"), T_transpose: T.Buffer((T.int64(4096), T.int64(4096)), "float16")):
+    T.func_attr({"op_pattern": 2, "tir.noalias": T.bool(True)})
+    # with T.block("root"):
+    decode = T.alloc_buffer((T.int64(4096), T.int64(4096)), "float16")
+    for i, j in T.grid(T.int64(4096), T.int64(4096)):
+        with T.block("decode"):
+            v_i, v_j = T.axis.remap("SS", [i, j])
+            T.reads(rxplaceholder[v_i // T.int64(8), v_j], rxplaceholder_1[v_i // T.int64(32), v_j], rxplaceholder_2[v_i // T.int64(32), v_j])
+            T.writes(decode[v_i, v_j])
+            decode[v_i, v_j] = T.Cast("float16", T.bitwise_and(T.shift_right(rxplaceholder[v_i // T.int64(8), v_j], T.Cast("uint32", v_i % T.int64(8) * T.int64(4))), T.uint32(15))) * rxplaceholder_1[v_i // T.int64(32), v_j] + rxplaceholder_2[v_i // T.int64(32), v_j]
+    for ax0, ax1 in T.grid(T.int64(4096), T.int64(4096)):
+        with T.block("T_transpose"):
+            v_ax0, v_ax1 = T.axis.remap("SS", [ax0, ax1])
+            T.reads(decode[v_ax1, v_ax0])
+            T.writes(T_transpose[v_ax0, v_ax1])
+            T_transpose[v_ax0, v_ax1] = decode[v_ax1, v_ax0]
+
+@T.prim_func
+def decode5_fp16(rxplaceholder: T.Buffer((T.int64(512), T.int64(11008)), "uint32"), rxplaceholder_1: T.Buffer((T.int64(128), T.int64(11008)), "float16"), rxplaceholder_2: T.Buffer((T.int64(128), T.int64(11008)), "float16"), T_transpose: T.Buffer((T.int64(11008), T.int64(4096)), "float16")):
+    T.func_attr({"op_pattern": 2, "tir.noalias": T.bool(True)})
+    # with T.block("root"):
+    decode = T.alloc_buffer((T.int64(4096), T.int64(11008)), "float16")
+    for i, j in T.grid(T.int64(4096), T.int64(11008)):
+        with T.block("decode"):
+            v_i, v_j = T.axis.remap("SS", [i, j])
+            T.reads(rxplaceholder[v_i // T.int64(8), v_j], rxplaceholder_1[v_i // T.int64(32), v_j], rxplaceholder_2[v_i // T.int64(32), v_j])
+            T.writes(decode[v_i, v_j])
+            decode[v_i, v_j] = T.Cast("float16", T.bitwise_and(T.shift_right(rxplaceholder[v_i // T.int64(8), v_j], T.Cast("uint32", v_i % T.int64(8) * T.int64(4))), T.uint32(15))) * rxplaceholder_1[v_i // T.int64(32), v_j] + rxplaceholder_2[v_i // T.int64(32), v_j]
+    for ax0, ax1 in T.grid(T.int64(11008), T.int64(4096)):
+        with T.block("T_transpose"):
+            v_ax0, v_ax1 = T.axis.remap("SS", [ax0, ax1])
+            T.reads(decode[v_ax1, v_ax0])
+            T.writes(T_transpose[v_ax0, v_ax1])
+            T_transpose[v_ax0, v_ax1] = decode[v_ax1, v_ax0]
+
+@T.prim_func
+def decode6_fp16(rxplaceholder: T.Buffer((T.int64(1376), T.int64(4096)), "uint32"), rxplaceholder_1: T.Buffer((T.int64(344), T.int64(4096)), "float16"), rxplaceholder_2: T.Buffer((T.int64(344), T.int64(4096)), "float16"), T_transpose: T.Buffer((T.int64(4096), T.int64(11008)), "float16")):
+    T.func_attr({"op_pattern": 2, "tir.noalias": T.bool(True)})
+    # with T.block("root"):
+    decode = T.alloc_buffer((T.int64(11008), T.int64(4096)), "float16")
+    for i, j in T.grid(T.int64(11008), T.int64(4096)):
+        with T.block("decode"):
+            v_i, v_j = T.axis.remap("SS", [i, j])
+            T.reads(rxplaceholder[v_i // T.int64(8), v_j], rxplaceholder_1[v_i // T.int64(32), v_j], rxplaceholder_2[v_i // T.int64(32), v_j])
+            T.writes(decode[v_i, v_j])
+            decode[v_i, v_j] = T.Cast("float16", T.bitwise_and(T.shift_right(rxplaceholder[v_i // T.int64(8), v_j], T.Cast("uint32", v_i % T.int64(8) * T.int64(4))), T.uint32(15))) * rxplaceholder_1[v_i // T.int64(32), v_j] + rxplaceholder_2[v_i // T.int64(32), v_j]
+    for ax0, ax1 in T.grid(T.int64(4096), T.int64(11008)):
+        with T.block("T_transpose"):
+            v_ax0, v_ax1 = T.axis.remap("SS", [ax0, ax1])
+            T.reads(decode[v_ax1, v_ax0])
+            T.writes(T_transpose[v_ax0, v_ax1])
+            T_transpose[v_ax0, v_ax1] = decode[v_ax1, v_ax0]
+
+
 def decode_sch_func(orig_func):
     sch = tvm.tir.Schedule(orig_func)
     b0 = sch.get_block(name="decode", func_name="main")
@@ -2492,6 +2547,9 @@ tir_dispatch_dict = {
     tvm.ir.structural_hash(decode6): decode_sch_func(decode6),
     tvm.ir.structural_hash(decode7): decode_sch_func(decode7),
     tvm.ir.structural_hash(decode8): decode_sch_func(decode8),
+    tvm.ir.structural_hash(decode4_fp16): decode_sch_func(decode4_fp16),
+    tvm.ir.structural_hash(decode5_fp16): decode_sch_func(decode5_fp16),
+    tvm.ir.structural_hash(decode6_fp16): decode_sch_func(decode6_fp16),
     tvm.ir.structural_hash(fused_decode3_matmul1_before): fused_decode3_matmul1_after,
     tvm.ir.structural_hash(fused_decode4_fused_matmul5_add3_before): fused_decode4_fused_matmul5_add3_after,
     tvm.ir.structural_hash(fused_decode4_matmul5_before): fused_decode4_matmul5_after,
